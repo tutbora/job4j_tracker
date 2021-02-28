5bdbb801d3e6586fbad9999eb506a499722de815
tutbora <tutbora@gmail.com>
2021-02-28 18:28:37 +0300

    3. Банковские переводы [#424929]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,62 @@
+package ru.job4j.bank;
+
+import java.util.*;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        ArrayList<Account> list = new ArrayList<>();
+        if (!users.containsKey(user)) {
+            users.put(user, list);
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+        User rsl = findByPassport(passport);
+        if (rsl != null) {
+            List<Account> list = users.get(rsl);
+            if (list != null) {
+                if (!list.contains(account)) {
+                    list.add(account);
+                }
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (var find : users.keySet()) {
+            if (find.getPassport().equals(passport)) {
+                return find;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User rsl = findByPassport(passport);
+        if (rsl != null) {
+            List<Account> accounts = users.get(rsl);
+            for (var userAccount : accounts) {
+                if (userAccount.getRequisite().contains(requisite)) {
+                    return userAccount;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+    var userSrcReq = findByRequisite(srcPassport, srcRequisite);
+    var userDestReq = findByRequisite(destPassport, destRequisite);
+    if (userSrcReq != null && userDestReq != null) {
+        if (userSrcReq.getBalance() >= amount) {
+            userSrcReq.setBalance(userSrcReq.getBalance() - amount);
+            userDestReq.setBalance(userDestReq.getBalance() + amount);
+            return true;
+        }
+    }
+        return false;
+    }
+}
