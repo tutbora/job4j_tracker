f7545afd4e64d19f3d14e7e41e6e1d8db228b4b0
tutbora <tutbora@gmail.com>
2021-03-26 16:43:30 +0300

    3. Что такое JavaDoc [#424906]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,27 +2,62 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * @author Z.Ruslan
+ * @version 1.0
+ * Class frame work with fields
+ */
 public class Account {
+
+    /**
+     * it is requisite for account number for stocks different currencies
+     */
     private String requisite;
+
+    /**
+     * it size of currency on account requisite
+     */
     private double balance;
 
+    /**
+     * it is account which contains data about requisite and balance
+     * @param requisite it is account number
+     * @param balance it is how much money are stock in account requisite
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Method lets get requisite account
+     * @return if by ask gives back requisite
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * @deprecated this method is not using yet,
+     * but it can set (create or change) requisite for account number
+     * @param requisite initialisation parameter of requisite
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Method gives back money size which on balance account
+     * @return if by ask gives balance
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * it sets (or change) balance for account requisite
+     * @param balance initialisation parameter of balance
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,15 +1,31 @@
 package ru.job4j.bank;
 
 import java.util.*;
-
+/**
+ * @author Z.Ruslan
+ * @version 1.0
+ * Class for service accounts of clients
+ */
 public class BankService {
+    /**
+     * This object contain database users with unique passports, accounts, requisites
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Method create a new user in library bank if did not find this client
+     * @param user accept user name
+     */
     public void addUser(User user) {
         ArrayList<Account> list = new ArrayList<>();
         users.putIfAbsent(user, list);
     }
 
+    /**
+     * Method create new account for user if did not find account which accepted
+     * @param passport unique passport
+     * @param account account in database
+     */
     public void addAccount(String passport, Account account) {
         User rsl = findByPassport(passport);
         if (rsl != null) {
@@ -20,6 +36,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Method find unique user passport in database
+     * @param passport unique passport
+     * @return user if found his passport
+     */
     public User findByPassport(String passport) {
         for (var find : users.keySet()) {
             if (find.getPassport().equals(passport)) {
@@ -29,6 +50,12 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Method return user account if found user passport and account requisite
+     * @param passport unique passport
+     * @param requisite user account
+     * @return user account
+     */
     public Account findByRequisite(String passport, String requisite) {
         User rsl = findByPassport(passport);
         if (rsl != null) {
@@ -42,6 +69,16 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Method for money transfer from 1 account requisite to
+     * 2 (this or other) account requisite and return true if this operation is true
+     * @param srcPassport unique user passport number 1
+     * @param srcRequisite user account requisite number 1
+     * @param destPassport unique user passport number 2
+     * @param destRequisite user account requisite number 2
+     * @param amount sum for money transfer
+     * @return true if this operation is true
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
     var userSrcReq = findByRequisite(srcPassport, srcRequisite);
