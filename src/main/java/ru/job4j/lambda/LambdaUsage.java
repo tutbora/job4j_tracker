7b3ed9a995c98301d8e9765fc25a1422b0a0215d
tutbora <tutbora@gmail.com>
2021-04-21 15:05:19 +0300

    2.2. Лямбда блок ! исправлено [#424744]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -4,10 +4,9 @@ import java.util.Comparator;
 
 public class LambdaUsage {
     public static void main(String[] args) {
-        Comparator<String> cmpDescSize = ((Comparator<String>) (left, right) -> {
+        Comparator<String> cmpDescSize = (left, right) -> {
             System.out.println("compare - " + left.length() + " : " + right.length());
-            return Integer.compare(left.length(), right.length());
-        }).reversed();
-
+            return Integer.compare(right.length(), left.length());
+        };
     }
 }
