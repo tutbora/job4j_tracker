c3dca58b8e9e31292a6f8d0da1db61669a04f2c9
tutbora <tutbora@gmail.com>
2021-04-23 08:17:12 +0300

    6. Функции высшего порядка !исправлено [#424738]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -17,26 +17,19 @@ public class PhoneDictionary {
      * @return Список подощедщих пользователей.
      */
 
-/*    public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
-            if (person.getName().contains(key)
-                    || person.getSurname().contains(key)
-                    || person.getPhone().contains(key)
-                    || person.getAddress().contains(key)) {
-                result.add(person);
-                //result.add(persons.get(i));
-            }
-        }
-        return result;
-    }
-*/
-
     public ArrayList<Person> find(String key) {
-        Predicate<Person> combine = person -> person.getName().contains(key)
-                || person.getSurname().contains(key)
-                || person.getPhone().contains(key)
-                || person.getAddress().contains(key);
+        Predicate<Person> combineName = person -> person.getName().contains(key);
+        Predicate<Person> combineSurname = person -> person.getSurname().contains(key);
+        Predicate<Person> combinePhone = person -> person.getPhone().contains(key);
+        Predicate<Person> combineAddress = person -> person.getAddress().contains(key);
+//        Predicate<Person> combine = combineName
+//                .or(combineSurname.
+//                        or(combinePhone.
+//                                or(combineAddress)));
+        Predicate<Person> combine = combineName
+                .or(combineSurname)
+                .or(combinePhone)
+                .or(combineAddress);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (combine.test(person)) {
