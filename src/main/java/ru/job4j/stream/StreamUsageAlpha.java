3a0a15d8e2e8e0c288875ae6cb5eefccf769ed9d
tutbora <tutbora@gmail.com>
2021-04-26 14:30:54 +0300

    0. Stream API !исправлено [#424751]

--- src/main/java/ru/job4j/stream/StreamUsageAlpha.java
+++ src/main/java/ru/job4j/stream/StreamUsageAlpha.java
@@ -2,18 +2,18 @@ package ru.job4j.stream;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 public class StreamUsageAlpha {
     public static void main(String[] args) {
         ArrayList<Integer> listNum = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            listNum.add(random.nextInt());
-        }
-        listNum.forEach(System.out::println);
-        System.out.println();
+        listNum.add(0, 5);
+        listNum.add(1, -15);
+        listNum.add(2, 7);
+        listNum.add(3, -4);
+        listNum.add(4, 9);
+        listNum.add(5, 2);
+
         List<Integer> listPositiveSorted = listNum.stream()
                 .filter(num -> (num > 0))
                 .collect(Collectors.toList());
