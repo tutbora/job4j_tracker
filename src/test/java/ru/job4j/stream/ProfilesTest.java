3b8babdbcd5c5567152e28744ba19c0ef2fb7973
tutbora <tutbora@gmail.com>
2021-05-10 19:15:01 +0300

    3. Уникальность элементов и сортировка. !исправлено [#424754]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.stream;
 
 import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,29 +13,17 @@ public class ProfilesTest {
 
     @Test
     public void whenCollectAddress() {
-        List<Address> input = List.of(
-                new Address("GA", "Garden Street", 86, 98),
-                new Address("NY", "Washington Street", 8, 123),
-                new Address("FI", "Brabus Street", 4, 75),
-                new Address("LA", "White Street", 5, 697),
-                new Address("GA", "Garden Street", 86, 98),
-                new Address("FI", "Brabus Street", 4, 75),
-                new Address("NY", "Washington Street", 8, 123),
-                new Address("GA", "Garden Street", 86, 98),
-                new Address("NY", "Washington Street", 8, 123),
-                new Address("FI", "Brabus Street", 4, 75)
-        );
-        List<Profile> profOne = List.of(
-                new Profile(input.get(0)),
-                new Profile(input.get(1)),
-                new Profile(input.get(2)),
-                new Profile(input.get(3)),
-                new Profile(input.get(4)),
-                new Profile(input.get(5)),
-                new Profile(input.get(6)),
-                new Profile(input.get(7)),
-                new Profile(input.get(8)),
-                new Profile(input.get(9))
+        List<Profile> input = Arrays.asList(
+                new Profile(new Address("GA", "Garden Street", 86, 98)),
+                new Profile(new Address("NY", "Washington Street", 8, 123)),
+                new Profile(new Address("FI", "Brabus Street", 4, 75)),
+                new Profile(new Address("LA", "White Street", 5, 697)),
+                new Profile(new Address("GA", "Garden Street", 86, 98)),
+                new Profile(new Address("FI", "Brabus Street", 4, 75)),
+                new Profile(new Address("NY", "Washington Street", 8, 123)),
+                new Profile(new Address("GA", "Garden Street", 86, 98)),
+                new Profile(new Address("NY", "Washington Street", 8, 123)),
+                new Profile(new Address("FI", "Brabus Street", 4, 75))
         );
         List<Address> expect = List.of(
                 new Address("FI", "Brabus Street", 4, 75),
@@ -41,7 +31,7 @@ public class ProfilesTest {
                 new Address("LA", "White Street", 5, 697),
                 new Address("NY", "Washington Street", 8, 123)
         );
-        List<Address> rsl = new Profiles().collect(profOne);
+        List<Address> rsl = new Profiles().collect(input);
         assertThat(rsl, is(expect));
     }
 }
