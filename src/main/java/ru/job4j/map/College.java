b7776cc92e6f2238db2f8147628cbf67d6d17ec9
tutbora <tutbora@gmail.com>
2021-05-16 21:13:00 +0300

    Temp

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -12,14 +12,10 @@ public class College {
     }
 
     public Optional<Student> findByAccountOpti(String account) {
-        Optional<Student> rsl = Optional.empty();
-        for (var s : students.keySet()) {
-            if (account.equals(s.getAccount())) {
-                rsl = Optional.of(s);
-                break;
-            }
-        }
-        return rsl;
+        return students.keySet()
+                .stream()
+                .filter(s -> account.equals(s.getAccount()))
+                .findFirst();
     }
 
     public Optional<Subject> findBySubjectNameOpti(String account, String name) {
@@ -27,12 +23,11 @@ public class College {
         var s = findByAccountOpti(account);
         if (s.isPresent()) {
             var subjects = students.get(s.get());
-            for (var subj : subjects) {
-                if (name.equals(subj.getName())) {
-                    rsl = Optional.of(subj);
-                    break;
-                }
-            }
+            rsl = subjects.stream()
+                    .filter(subj -> name.equals(subj.getName()))
+                    .findFirst()
+                    .map(Optional::of)
+                    .orElse(rsl);
         }
         return rsl;
     }
