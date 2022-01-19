f1c9348ed327b03b672d163c95fd06c2cfdd96a2
Rome <tutbora@gmail.com>
2022-01-19 13:03:29 +0300

    temp + fileReader

--- src/main/java/ru/job4j/temp/CalcDiagonal.java
+++ src/main/java/ru/job4j/temp/CalcDiagonal.java
@@ -8,14 +8,14 @@ public class CalcDiagonal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double pxlLength, smLength, pxlHeight, smHeight, inchDiagonal, smDiagonal, smStep;
-        print(2, "Расчет размера дисплея монитора.");
-        print(2, "Пример данных для расчета: разрешение 1920х1080, "
+        printLineOrNot(2, "Расчет размера дисплея монитора.");
+        printLineOrNot(2, "Пример данных для расчета: разрешение 1920х1080, "
                 + "диагональ 23.6\"");
-        print(1, "Введите длину в пикселях: ");
-        pxlLength = scanner.nextDouble();
-        print(1, "Введите высоту в пикселях: ");
+        printLineOrNot(1, "Введите длину в пикселях: ");
+        pxlLength = scanner.nextFloat();
+        printLineOrNot(1, "Введите ширину в пикселях: ");
         pxlHeight = scanner.nextDouble();
-        print(1, "Введите диагональ в дюймах: ");
+        printLineOrNot(1, "Введите диагональ в дюймах: ");
         inchDiagonal = scanner.nextDouble();
         smDiagonal = goToSm(inchDiagonal);
         double pxlDiagonal = Math.sqrt(pxlLength * pxlLength + pxlHeight * pxlHeight);
@@ -24,21 +24,28 @@ public class CalcDiagonal {
         smHeight = pxlHeight * smStep;
         double pxlsOnSm = pxlLength / smLength * pxlHeight / smHeight;
         double pxlsOnInch = goToSm(pxlLength / smLength) * goToSm(pxlHeight / smHeight);
-        System.out.println("Длина расчетная, см: " + round(smLength, 1));
-        print(2, "Высота расчетная, см: " + round(smHeight, 1));
-        print(2, "Диагональ расчетная, см: "
-                + round(Math.sqrt(smLength * smLength + smHeight * smHeight), 1));
-        print(2, "Пикселей на см2: " + round(pxlsOnSm, 0));
-        print(2, "Пикселей на дюйм2: " + round(pxlsOnInch, 0));
+        toConsole(smLength, smHeight, pxlsOnSm, pxlsOnInch, pxlLength, pxlHeight);
     }
 
-    private static void print(int line, String text) {
+    private static void printLineOrNot(int line, String text) {
         if (line == 1) {
             System.out.print(text);
-        }
-        if (line == 2) {
+        } else if (line == 2) {
             System.out.println(text);
+        } else {
+            System.out.println("Error input");
         }
+    }
+
+    private static void toConsole(double smLength, double smHeight, double pxlsOnSm,
+                                  double pxlsOnInch, double pxlLength, double pxlHeight) {
+        printLineOrNot(2, "Длина расчетная, см: " + round(smLength, 1));
+        printLineOrNot(2, "Ширина расчетная, см: " + round(smHeight, 1));
+        printLineOrNot(2, "Диагональ расчетная, см: "
+                + round(Math.sqrt(smLength * smLength + smHeight * smHeight), 1));
+        printLineOrNot(2, "Пикселей на см2: " + round(pxlsOnSm, 0));
+        printLineOrNot(2, "Пикселей на дюйм2: " + round(pxlsOnInch, 0));
+        printLineOrNot(2, "Всего пикселей: " + round(pxlLength * pxlHeight, 0));
     }
 
     private static double goToSm(double inch) {
--- src/main/java/ru/job4j/temp/DoubleLoop.java
+++ src/main/java/ru/job4j/temp/DoubleLoop.java
@@ -10,5 +10,28 @@ public class DoubleLoop {
                 .flatMap(level -> Stream.of(tasks)
                         .map(task -> level + " " + task))
                 .forEach(System.out::println);
+        System.out.println();
+        repeat(123);
+        repeat(255);
+        repeat(123456789);
+    }
+
+    public static String binary(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 31; i++) {
+            sb.append(num % 2 == 0 ? 0 : 1);
+            sb.append((i + 1) % 8 == 0 ? " " : "");
+            num /= 2;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void repeat(int num) {
+        System.out.println(hash(num));
+        System.out.println(binary(num));
+    }
+
+    public static int hash(Object key) {
+        return (key == null) ? 0 : key.hashCode() ^ (key.hashCode() >>> 16);
     }
 }
